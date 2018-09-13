55474381fac85e87498a35508926f8450b36191e
hednowley <13064635+hednowley@users.noreply.github.com>
2018-09-13 17:46:35 +0100

    Refactor layout

--- source/ts/components/cell.tsx
+++ source/ts/components/cell.tsx
@@ -3,25 +3,14 @@ import { Store } from "../redux/store";
 import { createValueChangedAction } from "../redux/actionCreators";
 import * as React from "react";
 import { createMouseEnterCellThunk, createMouseExitCellThunk, createMouseDownThunk } from "../redux/mouse";
+import { CellContent } from "./cellContent";
 
 interface Props {
 	id: string;
+	coordinate: number[]
 }
 
-interface ReduxProps {
-	coordinate: number[];
-	value: string;
-	focused: boolean;
-	editing: boolean;
-}
-
-interface DispatchProps {
-	onMouseEnter: () => void;
-	onMouseExit: () => void;
-	onValueChanged: (value: string) => void;
-}
-
-class CellComponent extends React.Component<Props & DispatchProps & ReduxProps> {
+export class Cell extends React.Component<Props> {
 
 	render() {
 		const style: React.CSSProperties = {
@@ -29,45 +18,13 @@ class CellComponent extends React.Component<Props & DispatchProps & ReduxProps> 
 			gridRow: this.props.coordinate[0] + 1 // Make space for the column header
 		};
 
-		let className = "table-cell";
-
-		if (this.props.focused) {
-			className += " focused";
-		}
-
 		return (
 			<div
-				className={className}
-				onMouseEnter={this.props.onMouseEnter}
-				onMouseLeave={this.props.onMouseExit}
+				className="table-cell"
 				style={style}
 			>
-				<div className="cell-contents">{this.props.value}</div>
+				<CellContent id={this.props.id}/>
 			</div>
 		);
 	}
 }
-
-const mapStateToProps: MapStateToProps<ReduxProps, Props, Store> = (store, ownProps) => {
-	const reduxCell = store.Cells[ownProps.id];
-	return {
-		coordinate: reduxCell.coordinate,
-		value: reduxCell.value,
-		editing: store.EditingCell == ownProps.id,
-		focused: store.FocusedCell == ownProps.id
-	};
-};
-
-const mapDispatchToProps: MapDispatchToProps<DispatchProps, Props & ReduxProps> = (
-	dispatch,
-	ownProps
-) => ({
-	onMouseEnter: () => dispatch<any>(createMouseEnterCellThunk(ownProps.id)),
-	onMouseExit: () => dispatch<any>(createMouseExitCellThunk()),
-	onValueChanged: value => dispatch(createValueChangedAction(value, ownProps.id))
-});
-
-export const Cell = connect<ReduxProps, DispatchProps, Props, Store>(
-	mapStateToProps,
-	mapDispatchToProps
-)(CellComponent);

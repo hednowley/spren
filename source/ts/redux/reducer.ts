fa13c589b9e61a67094ce40f984f6f9196f73fbd
nedhowley <ned.howley@oxfordcc.co.uk>
2018-09-13 18:52:28 +0100

    WIP

--- source/ts/redux/reducer.ts
+++ source/ts/redux/reducer.ts
@@ -6,19 +6,18 @@ const getId = (coordinate: number[]) => coordinate.join(".");
 
 export const reducer = (store: Store, action: AllActions): Store => {
 	switch (action.type) {
-
 		case ActionTypeKeys.SET_CURRENT_CELL: {
 			return {
 				...store,
 				CurrentCell: action.id
-			}
+			};
 		}
 
 		case ActionTypeKeys.SET_FOCUSED_CELL: {
 			return {
 				...store,
 				FocusedCell: action.id
-			}
+			};
 		}
 
 		case ActionTypeKeys.VALUE_CHANGED: {
@@ -31,6 +30,33 @@ export const reducer = (store: Store, action: AllActions): Store => {
 						value: action.value
 					}
 				}
+			};
+		}
+
+		case ActionTypeKeys.SET_COLUMN: {
+
+			var rowAxis = store.Axes.find(a => a.IsRow);
+
+			var layout: any[] = [];
+
+			Object.keys(store.Cells).forEach(id => {
+				var cell = store.Cells[id];
+				if (cell.coordinate[2] == 1) {
+					layout.push({
+						column: cell.coordinate[0],
+						row: cell.coordinate[1],
+						id: id
+					});
+				}
+			});
+
+			return {
+				...store,
+				Axes: store.Axes.map((axis, index) => ({
+					...axis,
+					IsColumn: index == action.axis
+				})),
+				Layout: store.Cells.filter(c => c.)
 			};
 		}
 	}

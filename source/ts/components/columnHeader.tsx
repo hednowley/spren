6ad21b27f08ea96a3984ef155b12541bf0d142c0
hednowley <13064635+hednowley@users.noreply.github.com>
2018-09-13 17:22:56 +0100

    column highlighting

--- source/ts/components/columnHeader.tsx
+++ source/ts/components/columnHeader.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
+import { MapStateToProps, connect } from "react-redux";
+import { Store } from "../redux/store";
 
 interface Props {
 	column: number;
 }
 
-export class ColumnHeader extends React.Component<Props> {
+interface ReduxProps {
+	focused: boolean
+}
+
+export class ColumnHeaderComponent extends React.Component<Props & ReduxProps> {
 	render() {
 		const style: React.CSSProperties = {
 			gridColumn: this.props.column + 1,
 			gridRow: 1
 		};
 
+		var className = "column-header";
+		if (this.props.focused) {
+			className += " focused";
+		}
+
 		return (
 			<div
-				className="column-header"
+				className={className}
 				style={style}
 			>
 				{this.props.column}
@@ -21,3 +32,13 @@ export class ColumnHeader extends React.Component<Props> {
 		);
 	}
 }
+
+const mapStateToProps: MapStateToProps<ReduxProps, Props, Store> = (store, ownProps) => {
+	return {
+		focused: store.FocusedCell != null && store.Cells[store.FocusedCell].coordinate[1] == ownProps.column
+	};
+};
+
+export const ColumnHeader = connect<ReduxProps, {}, Props, Store>(
+	mapStateToProps
+)(ColumnHeaderComponent);
